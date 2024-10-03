c5e21679f434eb2b2b4cd9733713a7ba9cc02e7f
KennedyLL54 <edwinlagua439@gmail.com>
2024-10-03 23:25:47 +0200

    Ejemplo13 - Añadir

---
+++ EEDD2024/Ejemplo13/Persona.cpp
@@ -0,0 +1,14 @@
+#include "Persona.hpp"
+
+Persona::Persona() //repertir cada que se haga un metodo
+{
+}
+Persona::edad()
+{
+    this -> 
+    }
+
+Persona::~Persona()
+{
+}
+
---
+++ EEDD2024/Ejemplo13/Persona.hpp
@@ -0,0 +1,15 @@
+#ifndef PERSONA_HPP
+#define PERSONA_HPP
+	
+class Persona
+{
+	bool 
+	public:
+		Persona(int edad);
+		~Persona();
+		bool genero; // 1 mujer 0 hombre
+		int edad;
+		char *dni;
+};
+
+#endif // PERSONA_HPP
